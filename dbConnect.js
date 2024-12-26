3c17423354406667372676e0b006e32d270f9d6a
Sreedev <155372199+Sreedev-Github@users.noreply.github.com>
2024-12-26 19:35:00 +0530

    Create dbConnect.js

---
+++ dbConnect.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/DBNAME`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGO DB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
