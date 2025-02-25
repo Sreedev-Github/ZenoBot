8372d02a741871762386eeed3487e9ea90e33fff
Sreedev-Github <sreedev0612@gmail.com>
2025-02-25 07:23:11 +0530

    style: update global styles and completed UI for hero section in Desktop Layout. Need to work on responsiveness next.

--- zenobot/src/app/home/page.js
+++ zenobot/src/app/home/page.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -8,12 +10,27 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ArrowBigDownIcon } from "lucide-react";
+import { CalendarIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { addDays, format } from "date-fns";
+import { DateRange } from "react-day-picker";
 
-function page() {
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+
+function Page() {
+  const [date, setDate] = useState({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  });
   return (
-    <div className="relative h-[90vh] flex flex-col gap-10 justify-center items-center bg-[url(/hero-bg.jpg)] bg-center bg-cover shadow-inner">
+    <div className="relative h-[90vh] flex flex-col gap-14 justify-center items-center bg-[url(/hero-bg.jpg)] bg-center bg-cover shadow-inner">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/50"></div>
 
@@ -21,38 +38,109 @@ function page() {
         Your Ultimate AI Travel Companion: Plan, Explore, Enjoy!
       </p>
 
-      <div className="relative mb-auto flex gap-5 min-w-[50%] bg-white bg-opacity-15 rounded-full p-4 shadow-lg">
+      <div className="relative mb-auto flex gap-8 min-w-[50%] bg-white bg-opacity-25 rounded-full p-2 shadow-lg pl-8">
         <Input
           type="text"
           placeholder="From"
-          className="bg-transparent text-white placeholder:text-white border-t-0 border-x-0 border-b-2 focus-visible:ring-0 focus-visible:ring-offset-0  focus-visible:outline-none border-white rounded-none"
+          className="
+            my-auto bg-transparent text-white placeholder:text-white
+            border-b-2 border-transparent
+            focus:border-b-white
+            transition-[border-color] duration-300 ease-linear
+            focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0
+          "
         />
+
+        <Separator className="max-w-10 rotate-90 my-auto bg-white" />
+
         <Input
           type="text"
           placeholder="To"
-          className="bg-transparent text-white placeholder:text-white border-t-0 border-x-0 border-b-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none border-white rounded-none"
+          className=" my-auto bg-transparent text-white placeholder:text-white
+                    border-t-0 border-x-0
+                     border-b-2 border-transparent
+                     focus:border-b-white
+                     transition-all duration-300 ease-linear
+                     focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
         />
 
-        {/* Dropdown for Days */}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="outline"
-              className="bg-transparent border-none text-white text-lg hover:bg-transparent hover:border-none hover:text-white focus-visible:ring-offset-0 focus-visible:ring-0 focus-visible:outline-none"
-            >
-              From <ArrowBigDownIcon size={24} />
-            </Button>
-          </DropdownMenuTrigger>
+        <Separator className="max-w-10 rotate-90 my-auto bg-white" />
 
-          <DropdownMenuContent className="w-56 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none">
-            <DropdownMenuLabel>From</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Something</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {/* Dropdown for Days */}
+        <div
+          className={cn(
+            "grid gap-2 bg-transparent",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "my-auto w-[300px] justify-start text-left font-normal bg-transparent border-none text-white text-lg hover:bg-transparent hover:border-none hover:text-white focus-visible:ring-offset-0 focus-visible:ring-0 focus-visible:outline-none",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} -{" "}
+                      {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+
+        {/* Plan Trip Button */}
+
+        <Button
+          variant="primary"
+          className="bg-white text-black rounded-full py-9 px-14 text-xl hover:bg-transparent hover:text-white transition-all duration-300 ease-linear border-2 border-transparent hover:border-white"
+        >
+          Plan Trip
+        </Button>
       </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
+
+{
+  /* <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="bg-transparent border-none text-white text-lg hover:bg-transparent hover:border-none hover:text-white focus-visible:ring-offset-0 focus-visible:ring-0 focus-visible:outline-none"
+            >
+              Duration <CalendarIcon className="mr-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-56 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none">
+            <DropdownMenuLabel>Number of Days</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Something</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu> */
+}
